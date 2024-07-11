49511bbfb408fb76f9e8516394b8f296d707695a
Rayssa <rayssa.lima1@escolar.ifrn.edu.br>
2024-07-11 07:48:57 -0300

    Questões atualizadas

--- Q1.py
+++
@@ -1,5 +0,0 @@
-a = 1
-while(a<101):
-    print(a)
-    a= a + 1
-print("Fim do programa")
---
+++ while.py
@@ -0,0 +1,25 @@
+#--- QUESTÃO 1 - COMPLETA ---
+
+#Escreva um programa que exiba os valores dos números de 1 até 100. Ao término o 
+#programa deverá exibir uma mensagem de encerramento informando que o programa 
+#terminou.
+
+#valor = 0
+#while valor < 100:
+#      valor +=1
+#      print(valor)
+#print("Programa Encerrado")
+
+
+#--- QUESTÃO 2 - COMPLETA ---
+
+#senha = "Rayssa"
+#usuario = str(input("digite sua senha: "))
+
+#while(senha != usuario):
+#    print("senha incorreta")
+#    usuario = input("digite uma nova senha: ")
+
+
+#print("seja bem vindo")
+#print("Fim da questão")
