add4c5618beb588f1eee4afa5ece49177afd532c
Rayssa <rayssa.lima1@escolar.ifrn.edu.br>
2024-07-11 08:55:33 -0300

    codigos atualizados

--- for.py
+++ for.py
@@ -1,4 +1,53 @@
-a = 1
-while(a<101):
-    print(a)
-    a= a + 1
+# --- Q1 COMPLETA ---
+
+#Questão 1
+
+#Escreva um programa que imprima na tela os seguintes valores:
+#2 4 6 8 10 12 ...
+
+#i = 0
+#for i in range (2 , 14 , 2):
+#    print(i)
+
+
+# --- Q2 COMPLETA ---
+
+#Questão 2
+
+#Escreva um programa que solicite do usuário um valor positivo e
+#imprima todos os números inteiros de 1 até o número digitado pelo
+#usuário. Ao término, informe que o programa foi encerrado.
+
+#i = int(input("digite seu valor: "))
+#for i in range (0,i):
+#    i+=1
+#    print(i)
+#print("Programa Encerrado")
+
+
+#--- Q3 COMPLETA ---
+
+#Q.3 Escreva um programa que solicite do usuário dois valores positivos
+#e imprima todos os números inteiros dentro desse intervalo. O
+#algoritmo deve armazenar cada valor em duas variáveis diferentes
+#(v_ini e v_fim). Ao término, informe que o programa foi encerrado.
+
+#v_ini = int(input("digite seu primeiro valor: "))
+#v_fim = int(input("digite seu segundo valor: "))
+
+#for i in range (v_ini, v_fim+1):
+#    print(i)
+#print("Fim do Programa")
+
+
+#--- Q4 COMPLETA ---
+
+#Q.4 Escreva um programa que solicite do usuário dois valores positivos e imprima
+#todos os números inteiros dentro desse intervalo excluindo-se o valor inicial do
+#intervalo e o valor final. Ao término, informe que o programa foi encerrado.
+
+#p_v = int(input("digite seu primeiro valor: "))
+#s_v = int(input("digite seu segundo valor: "))
+
+#for i in range(p_v +1 , s_v ):
+#    print(i)
--- while.py
+++ while.py
@@ -11,7 +11,32 @@
 #print("Programa Encerrado")
 
 
-#--- QUESTÃO 2 - COMPLETA ---
+# --- QUESTÃO 2 - INCOMPLETA ---
+
+#Escreva um programa que solicita ao usuário valores numéricos e realiza a soma
+#desses valores. Quando o usuário digitar o valor 0 o programa deverá exibir o valor do
+#somatório e encerrar o programa com uma mensagem de término do programa. O
+#usuário deverá ser informado no início do programa o que o programa faz e qual a
+#condição para encerramento do programa.
+
+
+#p_valor = int(input("digite seu valor: "))
+#s_valor = int(input("digite seu valor:; "))
+
+#while (p_valor != 0):
+#       print(p_valor)
+#while (s_valor != 0):
+#      print(s_valor)
+
+
+#--- QUESTÃO 3 - COMPLETA ---
+
+#Crie um programa que solicita ao usuário uma senha e em seguida compare esse valor
+#com uma senha armazenada em uma variável. Enquanto o usuário não acertar o valor
+#da senha o programa deverá solicitar a senha ao usuário. Quando o usuário acerta a
+#senha, o programa deverá encerrar exibindo uma mensagem encerramento e informar
+#que o usuário acertou a senha.
+
 
 #senha = "Rayssa"
 #usuario = str(input("digite sua senha: "))
@@ -23,3 +48,24 @@
 
 #print("seja bem vindo")
 #print("Fim da questão")
+
+
+
+#--- QUESTÃO 4 - INCOMPLETA ---
+
+#Escreva um programa que gere um número aleatório entre 1 e 100 e peça ao usuário
+#para adivinhar qual é o número. O programa deve continuar pedindo palpites até que
+#o usuário acerte o número. Ao final, o programa deve informar quantos palpites foram
+#necessários e informar que o programa encerrou.
+
+
+import random
+
+palpite= int(input("digite sua escolha : "))
+n_aleatorio = random.randint(1,100)
+
+while(n_aleatorio != palpite):
+        print(n_aleatorio)
+if (n_aleatorio == palpite):
+      palpite +=1
+print(palpite)
